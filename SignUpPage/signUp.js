2f4d465e1e8a52d993c2092804f3e463801726fe
Harsh Gupta <harshgupta88911@gmail.com>
2024-03-16 16:16:32 +0530

    add few function

--- SignUpPage/signUp.js
+++ SignUpPage/signUp.js
@@ -3,5 +3,23 @@
 const signUpbtn = document.getElementById("SignUpbtn");
 
 signUpbtn.addEventListener("click", function () {
-  location.href = "/LogInPage/logInPage.html";
+  if (phoneEmail == "" || fullName == "" || userName == "" || passWord1 == "") {
+    alert("Please fill out all fields");
+  } else if (
+    phoneEmail === "7667045966" &&
+    fullName === "Harsh Gupta" &&
+    userName === "harshgupta8031" &&
+    passWord1 === "gupta88911@"
+  ) {
+    // Redirect to home.html page
+    location.href = "/LogInPage/logInPage.html";
+  } else if (
+    phoneEmail !== "7667045966" ||
+    fullName !== "Harsh Gupta" ||
+    userName !== "harshgupta8031" ||
+    passWord1 !== "gupta88911@"
+  ) {
+    // Redirect to home.html page
+    alert("Please enter  valid credentials");
+  }
 });
