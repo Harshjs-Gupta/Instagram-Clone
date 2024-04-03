8fd3fd3bab2f091b6ac0e12ab6e934678f6e49bd
Harsh Gupta <harshgupta88911@gmail.com>
2024-04-03 14:40:47 +0530

    remove else line fron email validation

--- LogInPage/script.js
+++ LogInPage/script.js
@@ -5,12 +5,6 @@ document.querySelector("#LogIn").addEventListener("click", function (e) {
   var password = document.getElementById("password").value;
 
   if (email == "" || password == "") {
-    alert("Please fill out all fields");
-  } else if (email === "7667045966" && password === "gupta88911@") {
-    // Redirect to home.html page
     location.href = "/HomePage/home.html";
-  } else if (email !== "7667045966" || password !== "gupta88911@") {
-    // Redirect to home.html page
-    alert("Please enter  valid credentials");
   }
 });
